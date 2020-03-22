a4c68cde1913f7f945a117751578bb39e495b7d0
Robert Mill <r.mill@ntlworld.com>
2020-03-22 15:31:16 +0000

    initial commit 2

---
+++ startup/routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const homeMain = require("../routes/homeMain");
+
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const error = require("../middleware/error");
+
+module.exports = function(app) {
+  app.use(express.json());
+  app.use("/api/homeMain", homeMain);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use(error);
+};
