6614484f9e70477eee25bb0897f9b5fc5082e504
Robert Mill <r.mill@ntlworld.com>
2020-03-24 12:37:15 +0000

    added about column 3

---
+++ routes/about.js
@@ -0,0 +1,63 @@
+const validateObjectId = require("../middleware/validateObjectId");
+const auth = require("../middleware/auth");
+const admin = require("../middleware/admin");
+const { Abouts, validate } = require("../models/about");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const abouts = await Abouts.find().select("-__v");
+  res.send(abouts);
+});
+
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let about = new Abouts({
+    heading: req.body.heading,
+    body: req.body.body
+  });
+  about = await about.save();
+
+  res.send(about);
+});
+
+router.put("/:id", [auth, validateObjectId], async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const about = await Abouts.findByIdAndUpdate(
+    req.params.id,
+    { heading: req.body.heading, body: req.body.body },
+    {
+      new: true
+    }
+  );
+
+  if (!about)
+    return res.status(404).send("The about with the given ID was not found.");
+
+  res.send(about);
+});
+
+router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
+  const about = await Abouts.findByIdAndRemove(req.params.id);
+
+  if (!about)
+    return res.status(404).send("The about with the given ID was not found.");
+
+  res.send(about);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const about = await Abouts.findById(req.params.id).select("-__v");
+
+  if (!about)
+    return res.status(404).send("The about with the given ID was not found.");
+
+  res.send(about);
+});
+
+module.exports = router;
