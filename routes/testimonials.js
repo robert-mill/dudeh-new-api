b338d7120d8b1f821887d03c572dc342f17b2795
Robert Mill <r.mill@ntlworld.com>
2020-04-25 19:07:58 +0100

    added videos and testimonials 4

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -3,33 +3,33 @@ const validateObjectId = require("../middleware/validateObjectId");
 const admin = require("../middleware/admin");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
-const { Events, validate } = require("../models/videos");
+const { Testimonials, validate } = require("../models/testimonials");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const videos = await Events.find();
-  res.send(videos);
+  const testimonials = await Testimonials.find();
+  res.send(testimonials);
 });
 
 router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  let video = new Events({
+  let testimonial = new Testimonials({
     heading: req.body.heading,
     body: req.body.body,
     videoLink: req.body.videoLink,
   });
-  video = await video.save();
+  testimonial = await testimonial.save();
 
-  res.send(video);
+  res.send(testimonial);
 });
 
 router.put("/:id", [auth], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const video = await Events.findByIdAndUpdate(
+  const testimonial = await Testimonials.findByIdAndUpdate(
     req.params.id,
     {
       heading: req.body.heading,
@@ -38,23 +38,29 @@ router.put("/:id", [auth], async (req, res) => {
     },
     { new: true }
   );
-  if (!video)
-    return res.status(404).send("The video with the given ID was not found.");
-  res.send(video);
+  if (!testimonial)
+    return res
+      .status(404)
+      .send("The testimonial with the given ID was not found.");
+  res.send(testimonial);
 });
 
 router.delete("/:id", [auth, admin], async (req, res) => {
-  const video = await Events.findByIdAndRemove(req.params.id);
-  if (!video)
-    return res.status(404).send("The video with the given ID was not found.");
-  res.send(video);
+  const testimonial = await Testimonials.findByIdAndRemove(req.params.id);
+  if (!testimonial)
+    return res
+      .status(404)
+      .send("The testimonial with the given ID was not found.");
+  res.send(testimonial);
 });
 
 router.get("/:id", validateObjectId, async (req, res) => {
-  const video = await Events.findById(req.params.id).select("-__v");
-  if (!video)
-    return res.status(404).send("The video with the given ID was not found.");
-  res.send(video);
+  const testimonial = await Testimonials.findById(req.params.id).select("-__v");
+  if (!testimonial)
+    return res
+      .status(404)
+      .send("The testimonial with the given ID was not found.");
+  res.send(testimonial);
 });
 
 module.exports = router;
