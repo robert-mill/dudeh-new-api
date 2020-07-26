69915a52ef519cefd109c2a6d86c1c4656d6e28b
Robert Mill <r.mill@ntlworld.com>
2020-07-26 17:57:21 +0100

    added background collections 1

--- routes/bg.js
+++ routes/bg.js
@@ -2,7 +2,6 @@ const validateObjectId = require("../middleware/validateObjectId");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const { Bg, validate } = require("../models/bg");
-const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
