e2306850c25b8b360feae5a1786564c49e896cb5
Robert Mill <r.mill@ntlworld.com>
2020-10-07 15:07:24 +0100

    building cv 9

--- documents/index.js
+++ documents/index.js
@@ -31,5 +31,66 @@ module.exports = ({ cv, interests, qualifications, workexperiences }) => {
         </tr>`
       )
       .join("")}
+      ${
+        workexperiences.length &&
+        `<tr><td><h4Work Experience></h4></td></tr>` &&
+        Array(workexperiences.length)
+          .join(0)
+          .split(0)
+          .map(
+            (m, i) =>
+              workexperiences[i] &&
+              `<tr key=${workexperiences[i]._id}>
+            <td>
+              <div class="workexperiences-head">${workexperiences[i].heading}</div>
+              <div class="workexperiences-body">${workexperiences[i].body}</div>
+            </td>
+          </tr>`
+          )
+          .join("")
+      }
+
+          ${
+            qualifications.length > 0 &&
+            `<tr><td><h4>Qualifications</h4></td></tr>` &&
+            `<tr><td><table><tr><th>title</th><th>description</th><th>grade</th><th>locaton</th></tr>` &&
+            Array(qualifications.length)
+              .join(0)
+              .split(0)
+              .map(
+                (m, i) =>
+                  qualifications[i] &&
+                  `<tr key=${qualifications[i]._id}>
+              <td>${qualifications[i].title}</td>
+              <td>${qualifications[i].description}</td>
+              <td>${qualifications[i].grade}</td>
+              <td>${qualifications[i].location}</td>
+            </tr>`
+              )
+              .join("") &&
+            `</table><td></tr>`
+          }
+
+
+          ${
+            interests.length > 0 &&
+            `<tr><td><h4>Qualifications</h4></td></tr>` &&
+            Array(interests.length)
+              .join(0)
+              .split(0)
+              .map(
+                (m, i) =>
+                  interests[i] &&
+                  `<tr key=${interests[i]._id}>
+              <td>
+                <div class="interests-head">${interests[i].heading}</div>
+                <div class="interests-body">${interests[i].body}</div>
+              </td>
+            </tr>`
+              )
+              .join("")
+          }
+
+        
        </tbody></table></body></html>`}`;
 };
